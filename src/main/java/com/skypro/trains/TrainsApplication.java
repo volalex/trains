6c17fc1122b30f990d8aa1348e7afaec7fe8fff1
Alexey Volkov <alexey.volkov@maxoptra.com>
2023-04-06 21:31:49 +0200

    Initial commit

---
+++ src/main/java/com/skypro/trains/TrainsApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.trains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TrainsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/skypro/trains/TrainsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.trains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
